c3e0fc542a624d8bb1e1d34040e28a4d5a7f12e5
Kisame712 <kisamehoshigakiuhmw712@gmail.com>
2025-07-31 18:43:52 +0530

    Added basic plane movement

---
+++ AirLoop/Assets/Scripts/PlaneController.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+   
+    [Header("Speed controls")]
+    [SerializeField] private float maxForwardSpeed;
+    [SerializeField] private float speedIncrement;
+    [SerializeField] private float responsiveness;
+    private float responseModifier { get { return planeRb.mass / 10; } }
+    private float forwardSpeed;
+    private float turnAmount;  // along the same plane (left or right rudder)
+    private float tiltAmount;  // out of the plane (like the plane is flipping sideways)
+    private float liftAmount;  // out of plane (like the plane is making a loop literally)
+
+    Rigidbody planeRb;
+
+    private void Awake()
+    {
+        planeRb = GetComponent<Rigidbody>();
+    }
+
+
+    private void Update()
+    {
+        PlaneRotation();
+    }
+
+    private void PlaneRotation()
+    {
+        turnAmount = Input.GetAxis("Yaw");
+        tiltAmount = Input.GetAxis("Pitch");
+        liftAmount = Input.GetAxis("Roll");
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            forwardSpeed += speedIncrement;
+        }
+
+        else if (Input.GetKey(KeyCode.LeftControl))
+        {
+            forwardSpeed -= speedIncrement;
+        }
+
+        forwardSpeed = Mathf.Clamp(forwardSpeed, 0, maxForwardSpeed);
+     
+    }
+    
+    private void FixedUpdate()
+    {
+        MovePlaneRigidBody();
+    }
+
+    private void MovePlaneRigidBody()
+    {
+        planeRb.AddForce(transform.right * forwardSpeed * responseModifier);
+        planeRb.AddTorque(transform.up * turnAmount * responseModifier);
+        planeRb.AddTorque(-transform.right * liftAmount * responseModifier);
+        planeRb.AddTorque(transform.forward * tiltAmount * responseModifier);
+    }
+
+}
