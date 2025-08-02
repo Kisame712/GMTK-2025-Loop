effa50d9d6e2da371ebc63b40c51d7c81b733e35
Kisame712 <kisamehoshigakiuhmw712@gmail.com>
2025-08-02 18:29:35 +0530

    Added new assets and sound effects

--- AirLoop/Assets/Scripts/Ring.cs
+++ AirLoop/Assets/Scripts/Ring.cs
@@ -3,10 +3,12 @@ using System;
 public class Ring : MonoBehaviour
 {
     public static event EventHandler OnAnyRingDestroyed;
+    public GameObject ringEffect;
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
+            Instantiate(ringEffect, transform.position, transform.rotation);
             Destroy(gameObject);
             OnAnyRingDestroyed?.Invoke(this, EventArgs.Empty);
         }
